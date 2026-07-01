d8010c9c3298e1e17612388b69991236599d777b
Sannidhya Ray <145295410+SRay2005@users.noreply.github.com>
2026-07-02 00:53:26 +0530

    Time: 167 ms (23.19%), Space: 22.3 MB (64.24%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -2,11 +2,12 @@ class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         final=[]
         products.sort()
+        m=len(products)
         for i in range(len(searchWord)):
             search_str=searchWord[:i+1]
             temp=[]
             n=len(search_str)
-            for i in range(len(products)):
+            for i in range(m):
                 if search_str == products[i][:n]:
                     temp.append(products[i])
                 if len(temp)==3:
