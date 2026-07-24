83d1a64cd8bc14fc17c8605138f02b7a35afab6c
Sannidhya Ray <145295410+SRay2005@users.noreply.github.com>
2026-07-25 00:45:06 +0530

    Time: 601 ms (6.54%), Space: 19.8 MB (11.39%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -10,13 +10,13 @@ class Solution:
             if s[right] in need:
                 need[s[right]] -= 1
 
-            while all(need[c] <= 0 for c in need):
-                window = s[left:right+1]
+            while all(need[c] <= 0 for c in need): #will be true when need count values of all keys in need will be <=0. 
+                window = s[left:right+1] #we save this window
 
-                if ans == "" or len(window) < len(ans):
+                if ans == "" or len(window) < len(ans): # we check if this is the first window, we save it as the ans, or if it is shorter than the current shortest window
                     ans = window
 
-                if s[left] in need:
+                if s[left] in need: #we shrink ny moving left pointer until the need values break the loop, and then expand  again using the right pointer due to the outer loop
                     need[s[left]] += 1
                 left += 1
 
