31edaaddd6f7eadc2d76f7dd944fa84464310f59
Sannidhya Ray <145295410+SRay2005@users.noreply.github.com>
2026-07-28 20:17:28 +0530

    Time: 863 ms (6.93%), Space: 19.9 MB (59.9%) - LeetHub

---
+++ 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class Solution:
+    def shortestSubstrings(self, arr: List[str]) -> List[str]:
+        n = len(arr)
+        substrings = {}
+        ans = []
+
+        # Generate all unique substrings for each string
+        for i in range(n):
+            arrlen = len(arr[i])
+            subs = set()
+
+            for j in range(arrlen):
+                for z in range(j, arrlen):  
+                    subs.add(arr[i][j:z + 1])
+
+            substrings[i] = subs   # use index instead of string
+
+        # Find shortest unique substring
+        for i in range(n):
+            curr = None
+
+            for sub in substrings[i]:   # iterate over the set
+                count = 0
+
+                for s in arr:
+                    if sub in s:
+                        count += 1
+
+                if count == 1:
+                    if (
+                        curr is None
+                        or len(sub) < len(curr)
+                        or (len(sub) == len(curr) and sub < curr)
+                    ):
+                        curr = sub
+
+            ans.append("" if curr is None else curr)
+
+        return ans
