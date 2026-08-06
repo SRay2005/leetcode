62eb1c807d2c9eea99bee51e1f85a1595ef724c9
Sannidhya Ray <145295410+SRay2005@users.noreply.github.com>
2026-08-06 15:20:10 +0530

    Time: 0 ms (100%), Space: 22.3 MB (39.9%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,32 +6,50 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        elements=[-1]*10001
+        # elements=[0]*10001
+
+        # def dfs(node):
+        #     nonlocal elements
+        #     if node is None:
+        #         return
+            
+        #     elements[node.val]+=1
+
+        #     if node.left:
+        #         dfs(node.left)
+            
+        #     if node.right:
+        #         dfs(node.right)
+            
+
+            
+        # dfs(root)
+        # n=len(elements)
+        # for i in range(n):
+        #     if k==1 and elements[i]>0:
+        #         return i
+
+        #     if elements[i]>0:
+        #         k-=1
+        answer = None
 
         def dfs(node):
-            nonlocal elements
+            nonlocal k, answer
+
             if node is None:
                 return
-            
-            elements[node.val]+=1
 
-            if node.left:
-                dfs(node.left)
-            
-            if node.right:
-                dfs(node.right)
-            
+            dfs(node.left)
 
-            
+            k -= 1
+            if k == 0:
+                answer = node.val
+                return
+
+            dfs(node.right)
+
         dfs(root)
-        n=len(elements)
-        for i in range(n):
-            if k==1 and elements[i]>-1:
-                return i
-
-            if elements[i]>-1:
-                k-=1
-        
+        return answer
 
 
         
